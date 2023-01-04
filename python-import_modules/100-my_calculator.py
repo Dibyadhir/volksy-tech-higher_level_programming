1c075c85b4f6e3ea5466d66f2ffcf3f89b9b3bcb
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-04 03:44:40 -0800

    sixth

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     lst = argv[1:]
     if len(lst) == 3:
-        a, b, c =  lst
+        a, b, c = lst
         if b == "+":
             re = add(int(a), int(c))
         elif b == "-":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         elif b == "/":
             re = div(int(a), int(c))
         else:
-            print("Unknown operator. Available operators: +, -, * and / ")
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         print("{} {} {} = {}".format(a, b, c, re))
     else:
