d62e69a09598da3d5a9b1357a51a162fef8e24fb
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-05 03:09:24 -0800

    fourth

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list = my_list
     if idx < 0:
-        return list
+        return my_list
     elif idx > len(my_list) - 1:
-        return list
+        return my_list
     else:
-        return list
+        copy = [x for x in my_list]
+        copy[idx] = element
+        return (copy)
+    
