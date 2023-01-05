5fa1be02bb08f1f8f7e4256e84e5fa38108459a7
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-05 03:34:01 -0800

    fifth

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,4 +3,5 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i != "c" or i != "C":
-            print(i, end='')
+            new_string = new_string + 1
+            return new_string
