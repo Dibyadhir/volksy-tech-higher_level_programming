1d5cd00195cb6c4f43df6bef334ec2be178679fe
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-05 01:52:38 -0800

    zero

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    lst=my_list
+def print_list_integer(my_list = []):
+    lst = my_list
     for i in  lst:
-        print("{}".format(i))
+        print("{:d}" .format(i))
