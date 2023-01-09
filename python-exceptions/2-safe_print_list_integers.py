6429edc827c4a1adf2251ccb56bfe66a6553f84f
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-09 00:00:44 -0800

    two

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list, x):
         c = 0
         for i in range(x):
             if type(my_list[i]) == int:
-                print(my_list[i], end="")
+                print('{:d}'.format(my_list[i]), end="")
                 c += 1
         return c
     except Exception :
