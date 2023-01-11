a5a1adb3be73b38fa10dc05ecaa571168b6ab41a
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-11 01:53:10 -0800

    This is second commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,12 +5,11 @@
 class Square:
     """this is square"""
     def __init__(self, size=0):
-
         try:
             if type(size) != int:
-                raise TypeError
+                raise TypeError('size must be an integer')
             elif size < 0:
-                raise ValueError('size must be an integer')
+                raise ValueError('size must be >= 0')
             else:
                 self.__size = size
         finally:
