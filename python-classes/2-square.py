8f7827b23e0e4f3e15a7fb81736230ddbff14976
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-11 01:47:59 -0800

    This is second commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is classes and object"""
+
+
+class Square:
+    #this is square
+    def __init__(self, size=0):
+
+        try:
+            if type(size) != int:
+                raise TypeError
+            elif size < 0:
+                raise ValueError('size must be an integer')
+            else:
+                self.__size = size
+        finally:
+            pass
