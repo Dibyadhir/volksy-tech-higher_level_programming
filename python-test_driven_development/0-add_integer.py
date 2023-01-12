b75fcf9ff68f87427258aefd52ee5d7b117a2d47
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-12 04:35:27 -0800

    hello

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" This is python DocTest """
+
+
 def add_integer(a, b=98):
+    """ This is Documented """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b)!= float:
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+    return int(a) + int(b)
