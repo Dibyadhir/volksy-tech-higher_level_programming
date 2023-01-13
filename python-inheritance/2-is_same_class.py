e59592cab591a01ef2bbc0d4b522c30ff1309ace
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-12 23:28:20 -0800

    Two task

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module is documented """
+
+def is_same_class(obj, a_class):
+    """ method is documented """
+    if type(obj) == a_class:
+        return True
+    return False
