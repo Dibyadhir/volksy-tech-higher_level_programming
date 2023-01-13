30d2f2527e098acf8909dacf8385f5b35ec89642
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 03:08:53 -0800

    hello 1st task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this is module documentation """
+
+
+class MyList:
+    """ this is class documentation """
+
+    def print_sorted(self):
+        """ this is method documented """
+        print(sorted(self))
