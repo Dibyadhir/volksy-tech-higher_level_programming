181c7bc7060634f71fe799a9e975173e9b0c3576
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 04:00:48 -0800

    hello 9th task

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" this is method validation """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """ here method is documented """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ this is public method """
+        return self.__height * self.__width
+    
+    def __str__(self):
+        """ this is str constructure """
+        return ("[Rectangle] {}/{}".format(self.__height, self.__width))
