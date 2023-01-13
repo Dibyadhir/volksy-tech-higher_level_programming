d4294f9a67114ff952670e825416a8eb821a54e4
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-12 23:41:07 -0800

    THIRD TASK

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" the module is documented """
+
+
+def is_kind_of_class(obj, a_class):
+    """ the method is documented """
+     if isinstance(obj, a_class):
+        return True
+    return False
