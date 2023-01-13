b9b11ae88b14684050eb9500763db14fb5aeaa9e
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 04:40:16 -0800

    hello 10th task

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ this is method validation """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
