141d9958e2a095f993fdbc53b53959291f608727
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 01:13:39 -0800

    This is fourth

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is module docuentation """
+
+
+def inherits_from(obj, a_class):
+    """ This is module documentation """
+    if type(obj) == a_class and type(obj) != a_class:
+        return True
+    return False
