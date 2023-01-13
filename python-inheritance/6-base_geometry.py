dc03ed304c4ab1e2ea9941cab01d155a4e9c8903
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 01:33:35 -0800

    this is sixth

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is module documentation """
+
+
+class BaseGeometry:
+    """ This is class documentation """
+    def area(self):
+        """ this is public instance """
+        raise Exception("area() is not implemented")
