927f1ce923a921c7f649c4105e2c9695402b252e
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-13 01:19:37 -0800

    This is fourth

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ This is module documentation """
-    if type(obj) == a_class and type(obj) != a_class:
+    if issubclass(type(obj, a_class)) and type(obj) != a_class:
         return True
-    return False
+    else:
+        return False
