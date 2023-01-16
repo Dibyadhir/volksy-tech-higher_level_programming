f62c2ccf87f567f8d1495a1ccaaeaa77ce99699e
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-16 09:41:37 -0800

    this is eight commit

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -13,12 +13,12 @@ class Rectangle:
         type(self).number_of_instances += 1
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -43,13 +43,13 @@ class Rectangle:
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
-    
+
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the Rectangle with the greater area."""
@@ -60,25 +60,25 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
-    
+
     def __str__(self):
        """ this is constuctore """
         if self.__width == 0 or self.__height == 0:
             return ("")
-    
+
         rect = []
         for i in range(self.__height):
             [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-    
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
+        rect += ", " + str(self.__height) + "")
         return (rect)
-    
+
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
