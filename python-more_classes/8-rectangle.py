7b4d18326215b6a693a11ff076913849ad51be48
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-16 10:18:38 -0800

    8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,34 +1,86 @@
 #!/usr/bin/python3
-
+"""
+Rectangle Class
 """
 
-Class Module
 
-"""
+class Rectangle:
+    """Class of Rectangle"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+    number_of_instances = 0
+    print_symbol = "#"
 
+    """class defined"""
+    def _init_(self, width=0, height=0):
+        """Initialization method"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
 
+    @property
+    def width(self):
+        """Setter Method of width"""
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        """Getter Method of width"""
+        if isinstance(value, int) is False:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise TypeError('width must be >= 0')
+        self.__width = value
 
+    @property
+    def height(self):
+        """Setter Method of height"""
+        return self.__height
 
+    @height.setter
+    def height(self, value):
+        """Getter Method of height"""
+        if isinstance(value, int) is False:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise TypeError('height must be >= 0')
+        self.__height = value
 
+    def area(self):
+        """method to return area of width and height"""
+        return self._width * self._height
 
-class Rectangle(BaseGeometry):
+    def perimeter(self):
+        """method to return perimeter of width and height"""
+        if self._width == 0 or self._height == 0:
+            return 0
+        return (self._width + self._height) * 2
 
-    """Rectangle subclass"""
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle area"""
+        if isinstance(rect_1, Rectangle) is False:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if isinstance(rect_2, Rectangle) is False:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        return rect_1
 
+    def _str_(self):
+        """method string object"""
+        if self._width == 0 or self._height == 0:
+            return ""
+        for row in range(self.__height - 1):
+            print(str(self.print_symbol) * self.__width)
+        return str(str(self.print_symbol) * self.__width)
 
+    def _repr_(self):
+        """repr method"""
+        return ("Rectangle({}, {})".format(self._width, self._height))
 
-
-    def __init__(self, width, height):
-
-        """initialization method"""
-
-        self.integer_validator('width', width)
-
-        self.integer_validator('height', height)
-
-        self.__width = width
-
-        self.__height = height
+    def _del_(self):
+        """delete object/instance"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
