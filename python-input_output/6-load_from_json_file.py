2dceb2dcbc8ce9c51c102c398b765fc8afec5126
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 03:42:50 -0800

    this sixth task commited

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     '''this is load-from-json-file'''
-    with open(file, 'w') as fp:
+    with open(filename, 'r') as fp:
         return json.load(fp)
