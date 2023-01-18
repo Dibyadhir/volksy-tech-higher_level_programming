e49f370d919324b025fa16aaaceb432447e45b25
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 03:40:26 -0800

    this sixth task commited

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""here module is commited"""
+import json
+
+
+def load_from_json_file(filename):
+    '''this is load-from-json-file'''
+    with open(file, 'w') as fp:
+        return json.load(fp)
