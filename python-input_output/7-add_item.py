7f360218112f2b951ef492eae1671858eb78e61d
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 04:49:41 -0800

    this is my eight task commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,14 +2,14 @@
 """here module is documented"""
 import sys
 
+if __name__ = "__main__"
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
-    my_list.append(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+    try:
+        values = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        values = []
+    for i in sys.argv[1:]:
+        values.append(i)
+    save_to_json_file(values, 'add_item.json')
