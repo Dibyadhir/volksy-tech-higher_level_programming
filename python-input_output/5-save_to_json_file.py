ae580e69c4b0f4a4a53055b9cc7ebda1874c5130
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 03:26:37 -0800

    this is my fifth task commit

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""here module is commited"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''this is save to json file'''
+    with open(filename, 'w') as fp:
+       json.dump(my_obj, f)
