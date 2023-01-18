d2af517fb708fe492898c36b1b7faa665dd4fee3
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 02:49:01 -0800

    hello first task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,8 @@
 
 def write_file(filename="", text=""):
     '''this is write function'''
-    with open(filename, 'w', encoding="utf-8")as f:
-        print(len(f.read()))
+    line = 0
+    with open(filename, 'r', encoding="utf-8")as f:
+        for i in f:
+            line = line + 1
+    return line
