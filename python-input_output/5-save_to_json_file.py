1437ab714ea29f239b552745e8f8b3d9a5bd3261
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 03:29:41 -0800

    this is my fifth task commit

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''this is save to json file'''
     with open(filename, 'w') as fp:
-       json.dump(my_obj, fp)
+        json.dump(my_obj, fp)
