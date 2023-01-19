5596b29f5246acd13a34adb163822a1fbb94d722
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 23:46:19 -0800

    this is my tenth commit

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""here python module is docuented"""
+
+
+class Student:
+    """this is class documatation"""
+
+    def __init__(self, first_name, last_name, age):
+        """this is constructore"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self, attrs=None):
+        """this is json"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            x = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    x[i] = self.__dict__.get(i)
+            return x
