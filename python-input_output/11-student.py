69d3247c6c02a7f75676596d4d32c56d84a34fa0
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-18 23:50:11 -0800

    this is my eleventh task commit

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""here pyhton module is documented"""
+
+
+class Student:
+    """this is class documatation"""
+
+    def __init__(self, first_name, last_name, age):
+        """this is constructore"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self, attrs=None):
+        """this is json"""
+        for i in attrs:
+            if type(attrs) == list and type(i) == str:
+                return self.__dict__
+        
+    def reload_from_json(self, json):
+        """this is reload_from_json"""
+        for i in json.items():
+            for j in i:
+                setattr(self, i, j)
