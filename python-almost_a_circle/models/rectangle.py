7e7b2f3f52090d9842bccd07381ee22481c83f05
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-20 04:46:36 -0800

    this is my second commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,5 +91,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """it overriding the __str__ method"""
-        return "Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-        self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                self.width, self.height)
