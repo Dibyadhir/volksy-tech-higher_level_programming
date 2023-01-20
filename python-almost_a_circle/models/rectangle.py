a028b611ccb0644f1437fa4b24cfd4c29b9cb7f0
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-20 01:50:48 -0800

    this is my second commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,8 +2,21 @@
 """module is documented"""
 
 
+class Base:
+    """this is my class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """this is constructore"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
 class Rectangle(Base):
-    """ """
+    """this is rectangle class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
