a51d219406d8e3e687eb737d98e00cfcfe219e5a
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-22 10:10:03 -0800

    this is square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,11 +5,12 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """this is square"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """ this is new Square."""
-        super().__init__(size, size, x, y, id)
+        super().__init__(self, size, x, y, id)
 
     def __str__(self):
-        """Return the print() and str() representation of a Square."""
+        """representation of a Square."""
         return "[Square] ({}) {}/{} - {}".format
     (self.id, self.x, self.y,self.width)
