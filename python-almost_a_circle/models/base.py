bd10cb60fd6d41efd0ce982bb60a23ec2283b5a3
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-01-23 21:26:08 -0800

    this is bash

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -72,12 +72,12 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """creates an instance based on a dictionary"""
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return new
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        else:
+            new = cls(1)
+        new.update(**dictionary)
+        return new
 
     @classmethod
     def load_from_file(cls):
