5605c0db1a20745818c65fe693e77ec2461bfaec
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-01 21:20:33 -0800

    task_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- task_2
-CREATE DATABASE IF NOT EXIT hbtn_0d_2;
-CREATE USER IF NOT EXIT 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON user_0d_2.* TO 'user_0d_2'@'localhost';
