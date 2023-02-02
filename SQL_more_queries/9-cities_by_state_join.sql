c0d2d73ae187eba8e63696e869e29770428b22b3
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-02 03:36:20 -0800

    task-9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- task-9
 SELECT cities.id,cities.name,states.name
 FROM cities 
-INNER JOINS states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
