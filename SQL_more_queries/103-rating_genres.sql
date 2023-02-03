387064100db09a30f3f79c205898556ba91421ce
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-03 04:16:56 -0800

    task-20

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,2 +1,7 @@
 -- task-20
-SELECT name,SUM(rate) AS rating FROM tv_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id GROUP BY name ORDER BY rating DESC;
+SELECT name,SUM(rate) AS rating 
+FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+inner join tv_show_ratings on tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name 
+ORDER BY rating DESC;
