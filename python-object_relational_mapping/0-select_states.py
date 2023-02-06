428a62c5ee458c1b7e77950403bfb34e7e691f97
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-06 02:27:04 -0800

    task-0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """module documented"""
+
 import sys
 import MySQLdb
 
 
 if __name__ = "__main__":
-    db = MyAQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
-    conn = db.cursor()
-    conn.execute("SELECT * FROM ststes")
-    [print(state) for state in conn.fetchall()]
+    dbconn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
+    cur = conn.curser()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+    cur.close()
+    conn.close()
