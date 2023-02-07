4a7502731c68780452df208c049d16c97b3418fd
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-06 22:28:17 -0800

    taskzero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
     q = 'select * from states order by id'
     cur.execute(q)
     a = cur.fecthall()
