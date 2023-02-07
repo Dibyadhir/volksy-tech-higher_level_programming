1b980ec06a2e24be4de2d08670deb99d2624c7fc
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-06 23:32:12 -0800

    tasktwo

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" module is documented """
-import MySQLdb
+""" this is documented """
 import sys
+import MySQLdb
 
 
-
-if __name__ = "__main__":
-    dbconn = MySQLdb.connect(host="localhost", port=3306, 
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = dbconn.cursor()
+if __name__ == '__main__':
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cor = con.cursor()
     x = sys.argv[4]
     a = 'select * from states where name = {} order by id'.format(x)
     cur.execute(a)
