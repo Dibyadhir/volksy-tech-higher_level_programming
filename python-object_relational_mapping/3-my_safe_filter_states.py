521d61ca78b111b4c94e7a791850f166147c352d
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-08 21:32:21 -0800

    task-3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this is ORM """
+import MySQLdb
+import os
+
+
+if __name__ == '__main':
+    db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE Name = %s ORDER BY id,[sys.argv[4]]")
+    f = cur.fetchall()
+    for i in f:
+        print(i)
