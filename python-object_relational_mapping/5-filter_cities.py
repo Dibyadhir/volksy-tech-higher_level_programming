0e3e29bb0263c76cb3912763d78c2098d39b947c
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-10 04:17:42 -0800

    task-5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,9 @@ import MySQLdb
 if __name__ == '__main__':
     con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = con.cursor()
-    cur.execute('''select cities from states inner join cities
-            on state_id = states.id where states.name = x''')
+    x = sys.argv[4]
+    cur.execute('''select cities.name from states inner join cities
+            on state_id = states.id where states.name = '{}' '''.format(x))
     y = cur.fetchall()
     for i in y:
         print(i)
