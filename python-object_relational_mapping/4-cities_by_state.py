c639e3a8913b5cdc7e0c9173b966a7b02cdb497b
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-10 03:31:44 -0800

    fourth

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 
-__name__ == '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
     cur = db.cursor()
     cur.execute('select cities.id, cities.name, state.name from states 
