c172bc550860473b83d93d4e9b16523d5ed80bc1
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-10 03:30:52 -0800

    fourth

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this is ORM """
+import MySQLdb
+import sys
+
+
+__name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute('select cities.id, cities.name, state.name from states 
+            inner join cities on cities.state_id = states.id order by id')
+    y = cur.fetchall()
+    for i in y:
+        print(i)
