afbc02ece7ab7587cc256d1be771f6a55dfb2a92
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-13 04:44:38 -0800

    task-15

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" this is declarative _base """
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,12 +8,7 @@ Base = declarative_base()
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
-    Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
-    """
+    """ Represents a city for a MySQL """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
