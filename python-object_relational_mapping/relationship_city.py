63970b0a99baa6e0031c6c939c86da8dfbdb26dc
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-13 04:40:48 -0800

    task-15

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+""" this is declaretie_base"""
 
 Base = declarative_base()
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
-    Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
-    """
+    """Represents a city for a MySQL """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
