ebb6000e4a6760b57c765bd2ca8b11a71a7d5e70
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-12 23:49:59 -0800

    task-7

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ this is lazy_matrix"""
+    return (np.matmul(m_a, m_b):)
