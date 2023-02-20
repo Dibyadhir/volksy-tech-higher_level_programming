06ec0e4e298474fbdb31dbc84722073a992af9b0
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-20 03:02:07 -0800

    task-2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 a = process.argv
 if (a.length == 3){
-    console.log("Argument found")
+    console.log("Argument found");
 }
 else if (a.length > 2){
-    console.log("Arguments found")
+    console.log("Arguments found");
 }
 else{
-    console.log("No argument")
+    console.log("No argument");
 }
