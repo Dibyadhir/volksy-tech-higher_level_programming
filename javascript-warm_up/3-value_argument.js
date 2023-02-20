627a38aa8ff1be7d25e4999b4e4c3231ef93409b
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-20 03:53:53 -0800

    task-3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 a = process.argv[2]
-if (typeof a == 'undefined'){
-    console.log('No argument')
+if (typeof a === 'undefined'){
+    console.log('No argument');
 }
 else{
-    console.log(a)
+    console.log(a);
 }
