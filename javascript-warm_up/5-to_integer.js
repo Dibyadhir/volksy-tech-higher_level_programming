5abe9927e7e219d871566c43e8901b04d5d604d2
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-20 05:12:29 -0800

    task-5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,7 @@
 const a = Number.parseInt(process.argv[2]);
 // console.log(a, typeof(a))
 if (isNaN(a)) {
-  console.log("Not a number");
-}
-else {
-  console.log("My number:", a);
+  console.log('Not a number');
+} else {
+  console.log('My number:', a);
 }
