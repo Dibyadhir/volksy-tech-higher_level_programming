531c51fbb305f2a4b30a54daf50daaa4177c4750
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-20 23:39:48 -0800

    task-6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let text = "";
-for (let x of arr) {
-    text += x + "<br>";
+let text = '';
+for (const x of arr) {
+    text += x + '<br>';
 }
-console.log(text);
+console.log(x);
