fa68b8d4feadedfc674f17cf37fc27b6b7d301b6
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-21 01:19:46 -0800

    task-9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-function add() {
-  console.log(Number.parseInt(process.argv[2] + Number.parseInt(process.argv[3])));
+function add () {
+  console.log(Number.parseInt(process.argv[2]) + Number.parseInt(process.argv[3]));
 }
 add();
-
