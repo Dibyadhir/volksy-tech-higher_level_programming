a2728a777d83b6781899a21fc8c2019d2ca96e87
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-21 01:06:26 -0800

    task-8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = Number.parseInt(process.argv[2]);
 if (isNaN(x)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else if (x > 0) {
-    for (let i = 0; i < x; i++) {
-        console.log('X'.repeat(x));
+  for (let i = 0; i < x; i++) {
+      console.log('X'.repeat(x));
     }
 }
