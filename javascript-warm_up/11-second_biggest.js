5ce9a5f78fd4bcad90d2e8b8568f066f2eff98e1
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-22 02:07:33 -0800

    task-11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const x = process.argv.slice(2)
-//console.log(x, typeof x)
-if (x.length in [0,1] ){
-    console.log(0)
+const x = process.argv.slice(2);
+// console.log(x, typeof x)
+if (x.length in [0, 1]) {
+  console.log(0);
+} else {
+  for (const i in x) {
+    // console.log(i,typeof i)
+    x[i] = Number.parseInt(x[i]);
+  }
 }
-else{
-for (let i in x){
-    //console.log(i,typeof i)
-    x[i] = Number.parseInt(x[i])
-}}
-x.sort(function(a,b){return b-a})
+x.sort(function (a, b) { return b - a; });
 console.log(x[1]);
