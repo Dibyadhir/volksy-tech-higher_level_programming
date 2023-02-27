fcf25f094b7b865d709f3c25e3eb48543efdfb3e
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-26 21:45:58 -0800

    task-101

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,7 @@
-able File  11 lines (11 sloc)  249 Bytes
-
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
+const newDict = {};
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
