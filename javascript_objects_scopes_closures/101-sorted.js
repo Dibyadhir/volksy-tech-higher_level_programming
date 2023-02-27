64af21edc28eaf36152f0947782418ad5b4dc266
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-02-26 21:40:09 -0800

    advance task

---
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+able File  11 lines (11 sloc)  249 Bytes
+
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
