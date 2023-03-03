3deb426412cfc2e00d0fac7cc6b654accc34c1be
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-03-03 04:02:28 -0800

    advance task

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Task 16"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,8 +8,8 @@ from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
